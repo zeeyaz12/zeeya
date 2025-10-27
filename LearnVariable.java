8ddf48781ff58cb6faa230ad860ad4fc480805c5
zeeyaz12 <yumekoae77@gmail.com>
2025-10-27 20:16:35 +0700

    belajar

---
+++ LearnVariable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LearnVariable {
+    public static void main(String[] args) {
+        int umur;
+        try (Scanner ns = new Scanner(System.in) {
+            System.out.println("masukkan umur");
+            nUmur = ns.nextInt();
+            System.out.printIn("umur adalah" + nUmur);
+            ns.close();
+        }
+
+    }
+    }
